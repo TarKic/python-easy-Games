16d3026bc743824f1b8685494ca8500c997d1361
Juan Tarca <juan.tarca1@gmail.com>
2022-11-25 10:44:36 -0300

    B

--- Simple Games/Menu de opciones.py
+++ Simple Games/Menu de opciones.py
@@ -1,6 +1,8 @@
+import os
 print ("Bienvenido!")
 print ("Que archivo desea visitar? ")
 eleccion = int (input ("1 - Generador de contraseñas \n2 - Advinador de numeros \n3 - Piedra Papel o tijera\n"))
+os.system ("clear")
 if (eleccion == 1):
     print ("La contreseña generada es: ")
     import passwordGenerator
